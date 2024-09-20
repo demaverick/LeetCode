afdbfd0d1493fc26722e0c47d24379526f0b58a2
Ananth Deepak Sharma Nanduri <ananth.deepaksharma@gmail.com>
2024-09-20 05:40:08 -0700

    Daily commit

---
+++ src/58.LengthOfTheLastWord.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int currentWordLength = 0, maxWordLength = 0;
+        for (int i = 0; i< s.length(); i++){
+            if(s.charAt(i)== ' '){
+                if(currentWordLength != 0) {
+                    maxWordLength = currentWordLength;
+                }
+                currentWordLength = 0;
+            }
+            else{
+                currentWordLength++;
+            }
+        }
+        if(currentWordLength != 0){
+            maxWordLength = currentWordLength;
+        }
+        return maxWordLength;
+    }
+}
